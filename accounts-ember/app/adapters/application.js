3c1f3e56ea074920e170e8be1d69135a36cdea8a
David Collette <collettedj@gmail.com>
2015-04-04 22:54:05 -0700

    modify sails to use ssl

--- accounts-ember/app/adapters/application.js
+++ accounts-ember/app/adapters/application.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   coalesceFindRequests: true,   // these blueprints support coalescing (reduces the amount of requests)
   namespace: 'api/v1',               // same as API prefix in Sails config
-  host: 'http://localhost:1337' // Sails server
+  host: 'https://localhost:1337' // Sails server
 });
